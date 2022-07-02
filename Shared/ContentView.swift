cfac603e1f968e90c54176b20eee04789e8457e2
ParkYoungMin <tomy050213@naver.com>
2022-07-03 01:29:01 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 박영민 on 2022/07/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/check_caffeine_1App.swift
@@ -0,0 +1,17 @@
+//
+//  check_caffeine_1App.swift
+//  Shared
+//
+//  Created by 박영민 on 2022/07/03.
+//
+
+import SwiftUI
+
+@main
+struct check_caffeine_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
